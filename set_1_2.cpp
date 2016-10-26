62d19a2433712c2e702ea8356dc9da549cfe195b
Borin Ouch <borin.ouch@gmail.com>
2016-10-26 09:21:55 -0700

    Move xor function into own file

--- set_1_2.cpp
+++ set_1_2.cpp
@@ -6,18 +6,7 @@
 using namespace std::string_literals;
 
 #include "base64.h"
-
-byte_vector operator^ (const byte_vector& lhs, const byte_vector& rhs) {
-    assert(lhs.size() == rhs.size());
-    const int size = lhs.size();
-    byte_vector b(size);
-
-    for (int i = 0; i < size; i += 1) {
-        b[i] = lhs[i] ^ rhs[i];
-    }
-
-    return b;
-}
+#include "xor.hpp"
 
 int main() {
     auto hex_str = "1c0111001f010100061a024b53535009181c"s;
---
+++ xor.hpp
@@ -0,0 +1,19 @@
+#ifndef __XOR_HPP__
+#define __XOR_HPP__
+
+#include <cassert>
+#include "base64.h"
+
+byte_vector operator^ (const byte_vector& lhs, const byte_vector& rhs) {
+    assert(lhs.size() == rhs.size());
+    const int size = lhs.size();
+    byte_vector b(size);
+
+    for (int i = 0; i < size; i += 1) {
+        b[i] = lhs[i] ^ rhs[i];
+    }
+
+    return b;
+}
+
+#endif//__XOR_HPP__
