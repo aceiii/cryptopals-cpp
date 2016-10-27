5d7a8ee3c6aa0f897370608af93371c794f7da91
Borin Ouch <borin.ouch@gmail.com>
2016-10-27 12:20:31 -0700

    Remove is_ascii_predicate from set_1_6

--- set_1_6.cpp
+++ set_1_6.cpp
@@ -37,13 +37,6 @@ namespace {
     const byte_vector AsciiWhitelist = { '\0', '\r', '\n', '\t' };
 }
 
-struct is_ascii_predicate {
-    bool operator () (const byte& b) const {
-        return (b >= 1 && b <= 127) ||
-            find(begin(AsciiWhitelist), end(AsciiWhitelist), b) != end(AsciiWhitelist);
-    }
-};
-
 void test1() {
     auto str1 = "this is a test"s;
     auto str2 = "wokka wokka!!!"s;
