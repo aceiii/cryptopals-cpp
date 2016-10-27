af4996c3bc82321668ba138c35718176a4cec13a
Borin Ouch <borin.ouch@gmail.com>
2016-10-27 11:03:58 -0700

    Implement set 1, challenge 6 - discover key size

--- set_1_6.cpp
+++ set_1_6.cpp
@@ -1,11 +1,14 @@
 #include <cassert>
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 #include <cmath>
 #include <bitset>
 
 #include "byte_vector.hpp"
+#include "byte_freqency.hpp"
+#include "base64.hpp"
 
 using namespace std::string_literals;
 
@@ -30,14 +33,47 @@ size_t hamming_distance(const T& t1, const T& t2) {
     return dist;
 }
 
-int main() {
-    auto keysize = 2;
+void test1() {
     auto str1 = "this is a test"s;
     auto str2 = "wokka wokka!!!"s;
-
     auto ham = hamming_distance(str1, str2);
 
     std::cout << ham << std::endl;
+}
+
+
+int main() {
+    byte_vector lines;
+    auto filename = "6.txt"s;
+    std::ifstream file(filename);
+    while (!file.fail()) {
+        std::string line;
+        file >> line;
+
+        auto b64_line = base64_to_bytes(line);
+        copy(begin(b64_line), end(b64_line), std::back_inserter(lines));
+    }
+
+    const int max_keysize = 40;
+    float min_ham = std::numeric_limits<float>::max();
+    int actual_keysize = 0;
+    for (int keysize = 2; keysize <= max_keysize; keysize += 1) {
+        byte_vector block1;
+        byte_vector block2;
+
+        copy(begin(lines), begin(lines) + keysize, back_inserter(block1));
+        copy(begin(lines) + keysize, begin(lines) + (2*keysize), back_inserter(block2));
+
+        auto ham = hamming_distance(block1, block2);
+        float normalized_ham = (float)ham / (float)keysize;
+
+        if (normalized_ham < min_ham) {
+            min_ham = normalized_ham;
+            actual_keysize = keysize;
+        }
+    }
+
+    std::cout << "Keysize: " << actual_keysize << std::endl;
 
     return 0;
 }
