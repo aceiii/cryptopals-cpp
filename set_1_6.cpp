9b2cd0e90bda8e10db34a2d5982d66722bb9762e
Borin Ouch <borin.ouch@gmail.com>
2016-10-27 12:13:12 -0700

    Finish up set 1, challenge 6 - fix keysize discovery, decrypt file

--- set_1_6.cpp
+++ set_1_6.cpp
@@ -33,6 +33,17 @@ size_t hamming_distance(const T& t1, const T& t2) {
     return dist;
 }
 
+namespace {
+    const byte_vector AsciiWhitelist = { '\0', '\r', '\n', '\t' };
+}
+
+struct is_ascii_predicate {
+    bool operator () (const byte& b) const {
+        return (b >= 1 && b <= 127) ||
+            find(begin(AsciiWhitelist), end(AsciiWhitelist), b) != end(AsciiWhitelist);
+    }
+};
+
 void test1() {
     auto str1 = "this is a test"s;
     auto str2 = "wokka wokka!!!"s;
@@ -60,11 +71,22 @@ int main() {
     for (int keysize = 2; keysize <= max_keysize; keysize += 1) {
         byte_vector block1;
         byte_vector block2;
+        byte_vector block3;
+        byte_vector block4;
 
         copy(begin(lines), begin(lines) + keysize, back_inserter(block1));
         copy(begin(lines) + keysize, begin(lines) + (2*keysize), back_inserter(block2));
+        copy(begin(lines) + (2*keysize), begin(lines) + (3*keysize), back_inserter(block3));
+        copy(begin(lines) + (3*keysize), begin(lines) + (4*keysize), back_inserter(block4));
 
-        auto ham = hamming_distance(block1, block2);
+        auto ham1 = hamming_distance(block1, block2);
+        auto ham2 = hamming_distance(block2, block3);
+        auto ham3 = hamming_distance(block3, block4);
+        auto ham4 = hamming_distance(block1, block3);
+        auto ham5 = hamming_distance(block1, block4);
+        auto ham6 = hamming_distance(block2, block4);
+        auto ham = (ham1 + ham2 + ham3 + ham4 + ham5 + ham6) / 6.0f;
+
         float normalized_ham = (float)ham / (float)keysize;
 
         if (normalized_ham < min_ham) {
@@ -75,6 +97,40 @@ int main() {
 
     std::cout << "Keysize: " << actual_keysize << std::endl;
 
+    std::vector<byte_vector> blocks (actual_keysize);
+    for (int i = 0; i < lines.size(); i += 1) {
+        blocks[i % actual_keysize].push_back(lines[i]);
+    }
+
+    byte_vector key(actual_keysize);
+    std::vector<byte_vector> decoded_blocks(actual_keysize);
+
+    for (int i = 0; i < actual_keysize; i += 1) {
+        const byte_vector& block = blocks[i];
+
+        auto freq = map_byte_frequency(block);
+        auto freq_vec = map_to_vector(freq);
+
+        sort_freq_vec_desc(freq_vec);
+
+        decoded_blocks[i] = block ^ freq_vec[0].first;
+        key[i] = freq_vec[0].first;
+    }
+
+    byte_vector decoded_lines(lines.size());
+    for (int i = 0; i < decoded_lines.size(); i += 1) {
+        decoded_lines[i] = decoded_blocks[i % actual_keysize][i / actual_keysize];
+    }
+
+    /* hmm lots of \0 and \x0c in the final output */
+    std::replace(begin(decoded_lines), end(decoded_lines), '\0', ' ');
+    std::replace(begin(decoded_lines), end(decoded_lines), '\x0c', '\n');
+
+    std::cout << "Key: " << bytes_to_str(key) << std::endl;
+    std::cout << bytes_to_str(decoded_lines) << std::endl;
+    //std::cout << decoded_lines << std::endl;
+
     return 0;
 }
+
 
