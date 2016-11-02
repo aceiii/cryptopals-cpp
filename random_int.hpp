2a5cd3974ae7f0fefc9bbba13de1f93f67a177a9
Borin Ouch <borin.ouch@gmail.com>
2016-11-02 05:01:49 -0700

    Refactor random_bytes into more generic class

---
+++ random_int.hpp
@@ -0,0 +1,30 @@
+#ifndef __RANDOM_INT_HPP__
+#define __RANDOM_INT_HPP__
+
+template <typename T>
+class random_int
+{
+    std::random_device rd;
+    std::mt19937 gen;
+    std::uniform_int_distribution<T> dist;
+
+public:
+    random_int(T lower, T upper):
+        rd(),gen(rd()),
+        dist(lower, upper)
+    {
+    }
+
+    T next() {
+        return dist(gen);
+    }
+
+    std::vector<T> next_n(size_t len) {
+        std::vector<T> result(len);
+        std::generate(begin(result), end(result), std::bind(&random_int::next, this));
+
+        return result;
+    }
+};
+
+#endif//__RANDOM_INT_HPP__
