a74afd6c438bc46b59f21ec3735e0a25312c76b6
Borin Ouch <borin.ouch@gmail.com>
2016-11-02 05:05:22 -0700

    Set 2, challenge 11 - Implement encryption oracle

--- set_2_11.cpp
+++ set_2_11.cpp
@@ -9,15 +9,64 @@
 #include "aes.hpp"
 #include "random_bytes.hpp"
 
+byte_vector encryption_oracle(const byte_vector& bytes) {
+    random_int<int> rand_count(5, 10);
+    random_bytes rand_bytes;
+    random_int<int> rand_coin(0, 1);
+
+    byte_vector rand_preprend = rand_bytes.next_n(rand_count.next());
+    byte_vector rand_append = rand_bytes.next_n(rand_count.next());
+
+    int remainder = (aes::block_size - ((bytes.size() + rand_preprend.size() + rand_append.size()) % aes::block_size)) % aes::block_size;
+
+    byte_vector new_bytes(bytes.size() + rand_preprend.size() + rand_append.size() + remainder);
+
+    auto start = begin(new_bytes);
+    std::copy(begin(rand_preprend), end(rand_preprend), next(start, 0));
+    std::copy(begin(bytes), end(bytes), next(start, rand_preprend.size()));
+    std::copy(begin(rand_append), end(rand_append), next(start, rand_preprend.size() + bytes.size()));
+
+    byte_vector rand_key = rand_bytes.next_n(aes::block_size);
+    byte_vector rand_iv = rand_bytes.next_n(aes::block_size);
+    byte_vector output;
+
+    if (rand_coin.next() == 0) {
+        output = aes_ecb_encrypt(new_bytes, rand_key);
+    } else {
+        output = aes_cbc_encrypt(new_bytes, rand_key, rand_iv);
+    }
+
+    return output;
+}
+
+byte_vector encryption_oracle(const std::string& str) {
+    return encryption_oracle(str_to_bytes(str));
+}
+
+void test1() {
+    constexpr int LO = 5;
+    constexpr int HI = 10;
+
+    random_int<int> rand_count(LO, HI);
+    int upper = std::numeric_limits<int>::min();
+    int lower = std::numeric_limits<int>::max();
+
+    constexpr size_t N = 10000;
+    for (int i = 0; i < N; i += 1) {
+        int rand_val = rand_count.next();
+        upper = std::max(upper, rand_val);
+        lower = std::min(lower, rand_val);
+    }
+
+    assert(lower == LO);
+    assert(upper == HI);
+
+    std::cout << "Succcess" << std::endl;
+}
+
 int main() {
-
-    random_bytes rand;
-
-    constexpr size_t block_size = 16;
-
-    byte_vector rand_key = rand.next_n_bytes(block_size);
-
-    std::cout << rand_key << std::endl;
+    byte_vector enc = encryption_oracle("You're weakenin' fast, YO! and I can tell it!wtf");
+    std::cout << enc << std::endl;
 
     return 0;
 }
