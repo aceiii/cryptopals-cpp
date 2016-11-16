3d5c8c6c26807a66fbd04d4e34d5ee2c8d43beb0
Borin Ouch <borin.ouch@gmail.com>
2016-11-16 12:36:34 -0800

    Break cbc using padding oracle for set 3, challenge 17

--- set_3_17.cpp
+++ set_3_17.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
+#include <set>
 
 #include "byte_vector.hpp"
 #include "aes.hpp"
 #include "random_bytes.hpp"
 #include "padding.hpp"
 #include "base64.hpp"
+#include "blocks.hpp"
 
 namespace {
     std::vector<std::string> rand_strings = {
@@ -54,6 +57,41 @@ bool decryption_oracle(const std::pair<byte_vector, byte_vector>& ciphertext_iv_
     return has_valid_pkcs7_padding(dec);
 }
 
+byte_vector solve_ciphertext(const byte_vector& block, const byte_vector& iv, const byte_vector& partial_solve = byte_vector(aes::block_size, 0)) {
+    constexpr size_t block_size = aes::block_size;
+
+    byte_vector current_block(block_size, 0);
+
+    for (int pos = block_size - 1; pos >= 0; pos -= 1) {
+        int i = 1;
+        int b = block_size - pos;
+
+        for (; i < 256; i += 1) {
+
+            byte_vector new_iv = iv;
+            for (int j = block_size - 1; j > pos; j -= 1) {
+                new_iv[j] = new_iv[j] xor (b xor current_block[j]);
+            }
+            new_iv[pos] = new_iv[pos] xor i;
+
+            auto pair = std::make_pair(block, new_iv);
+
+            bool is_valid = decryption_oracle(pair);
+
+            if (is_valid) {
+                current_block[pos] = b ^ i;
+                break;
+            }
+        }
+
+        if (i > 255) {
+            current_block[pos] = b;
+        }
+    }
+
+    return current_block;
+}
+
 void test1() {
     auto enc = random_encrypt();
 
@@ -64,7 +102,35 @@ void test1() {
 }
 
 int main() {
-    test1();
+
+    constexpr size_t block_size = aes::block_size;
+
+    auto enc = random_encrypt();
+    const byte_vector& ciphertext = enc.first;
+    const byte_vector& iv = enc.second;
+
+    std::vector<byte_vector> blocks = split_blocks(ciphertext, block_size);
+    std::vector<byte_vector> decrypted_blocks;
+
+    for (int i = 0; i < blocks.size(); i += 1) {
+        if (i == 0) {
+            decrypted_blocks.push_back(solve_ciphertext(blocks[0], iv));
+        } else {
+            decrypted_blocks.push_back(solve_ciphertext(blocks[i], blocks[i-1]));
+        }
+    }
+
+    byte_vector decrypted = combine_blocks(decrypted_blocks);
+    byte_vector plaintext;
+
+    strip_pkcs7_padding(decrypted, plaintext);
+
+    std::cout << "plaintext: " << bytes_to_str(plaintext) << std::endl;
+    std::cout << "bytes (hex): " << std::endl;
+    for (auto it = begin(decrypted_blocks); it != end(decrypted_blocks); it++) {
+        std::cout << "    " << bytes_to_hex(*it) << std::endl;
+    }
+
     return 0;
 }
 
