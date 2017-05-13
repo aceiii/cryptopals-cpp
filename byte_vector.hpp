f839c8e87348a377d45c9dac926d43d10ef1ce2e
Borin Ouch <borin.ouch@gmail.com>
2017-05-12 18:45:00 -0700

    Simplify bytes_to_str()

--- byte_vector.hpp
+++ byte_vector.hpp
@@ -9,13 +9,7 @@ typedef unsigned char byte;
 typedef std::vector<unsigned char> byte_vector;
 
 std::string bytes_to_str(const byte_vector& v) {
-    std::stringstream ss;
-
-    for (int i = 0; i < v.size(); i += 1) {
-        ss << v[i];
-    }
-
-    return ss.str();
+    return std::string(begin(v), end(v));
 }
 
 byte_vector str_to_bytes(const std::string& s) {
